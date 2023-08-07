5fd059c8f201bc52cc2d15e52950267cbde5bb6c
doishimpeiu <gazadaba38@yahoo.co.jp>
2023-08-07 13:49:51 +0900

    first commit

---
+++ C++/1.0.1.cpp
@@ -0,0 +1,13 @@
+/*
+キーポイント
+#include <bits/stdc++.h>とusing namespace std;は毎回最初に書く
+C++のプログラムはmain関数から始まる
+cout << "文字列" << endl;で文字列を出力できる
+プログラムは基本半角文字で書く
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "Hello, world!" << endl;
+}
